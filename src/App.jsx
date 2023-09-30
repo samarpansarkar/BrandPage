e798481f8f61bb369505a05c27be753fb4ab6d96
samarpan sarkar <samarpansarkar209@gmail.com>
2023-09-30 19:02:27 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-//import './App.css'
+import './App.css'
 import Navbar from './Components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='App'>
     <Navbar/>
       hello world
-    </>
+    </div>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Classes from './navbar.module.css';
 import logo from '../assets/brand_logo.png';
+
 
 const Navbar = () => {
   return (
     <React.Fragment>
-      <div className="navbar_container">
-        <div className="navbar_container-logo">
+      <div className={Classes.navbar_container}>
+        <div className={Classes.navbar_logo}>
             <img src={logo} alt="logo" />
+        </div>
+        <div className={Classes.navbar_options}>
+            menu
+        </div>
+        <div className="navbar-login">
+            <button>Login</button>
         </div>
       </div>
     </React.Fragment>
